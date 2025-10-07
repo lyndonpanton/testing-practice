b91743387e76e1a701e50cdeec25e75f0d709950
Lyndon Panton <LyndonPanton@gmail.com>
2025-10-07 01:57:05 +0100

    Feature: Passed all tests for caesar cipher function

--- src/js/caesar-cipher/caesar-cipher.js
+++ src/js/caesar-cipher/caesar-cipher.js
@@ -1,5 +1,27 @@
 function caesarCipher(string, shiftFactor) {
-    return "";
+    let result = "";
+    shiftFactor %= 26;
+    shiftFactor = shiftFactor < 0 ? shiftFactor * -1 : shiftFactor;
+
+    for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+            let newCharCode = string.charCodeAt(i) + shiftFactor;
+            
+            if (newCharCode > 90) newCharCode -= 26
+            
+            result += String.fromCharCode(newCharCode);
+        } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+            let newCharCode = string.charCodeAt(i) + shiftFactor;
+            
+            if (newCharCode > 122) newCharCode -= 26;
+
+            result += String.fromCharCode(newCharCode);
+        } else {
+            result += string[i];
+        }
+    }
+    
+    return result;
 }
 
 module.exports = caesarCipher;
